9d171fff648b7b7e3a61f90bc7de3480af3a82e8
LAPTOP-7JI17SMK\Richard Brown Thomse <richard-sej@live.dk>
2018-01-17 10:09:05 +0100

    Main menu is working

--- HangTheMan/HangTheMan/Main Menu.cs
+++ HangTheMan/HangTheMan/Main Menu.cs
@@ -6,7 +6,31 @@ using System.Threading.Tasks;
 
 namespace HangTheMan
 {
-    class Main_Menu
+    class Main_Menu : PrintStuff
     {
+        public string Welcome = "Welcome to hang the man \n" +
+            "!Start \n" +
+            "!Rules \n" +
+            "!Credits \n";
+
+
+        public string Rules = "RULES: \n" +
+            "You'll try to guess the word, one letter at a time \n" +
+            "if the letter is not in the word you'll lose a life \n" +
+            "if the letter is in the word you'll see where in the word it is \n" +
+            "you can't use the same letter more then one time. \n" +
+            "---------------------------------------------------------------------";
+
+        public string Credits = "Credits: \n" +
+            "The game was made by: Richard Brown Thomsen\n" +
+            "---------------------------------------------------------------------";
+
+
+        public string print(string input)
+        {
+            Console.WriteLine(input);
+            return input;
+        }
+
     }
 }
